2caa0667bca4051efd39a89bb1eb73bab2d1991a
Shubham0214 <shubhedu0112@gmail.com>
2025-07-30 16:34:44 +0530

    Sign up and login complete

---
+++ src/app/login/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { log } from "console";
+import toast from "react-hot-toast";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+
+  const [loading, setLoading] = React.useState(false);
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      await axios.post("/api/users/login", user);
+      console.log("Login success");
+      toast.success("Login Success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Login</h1>
+      <hr />
+      <label htmlFor="email">E-mail</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none"
+        id="email"
+        type="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <button
+        onClick={onLogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        Login Here
+      </button>
+      <Link href="/signup">Visit Sign Up</Link>
+    </div>
+  );
+}
---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,20 @@
+import { log } from "console";
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    mongoose.connect(process.env.mongo_url!);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("MongoDB connected Successfully");
+    });
+
+    connection.on("error", (err) => {
+      console.log("MongoDB connected successfully");
+    });
+  } catch (error) {
+    console.log("Something goes wrong!");
+    console.log(error);
+  }
+}
